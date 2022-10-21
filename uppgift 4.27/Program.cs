dc2e26c19b3c8eb129081e86f0dbe961ce5f2d11
TheBlueSoul5188 <hibba04@hotmail.se>
2022-10-21 11:04:53 +0200

    Add project files.

---
+++ uppgift 4.27/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uppgift_4_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Skriv nu in ett tal så många steg åt höger meddelandet ska stå");
+            int steg = int.Parse(Console.ReadLine());
+
+
+            for(int i = 0; i < meddelande.Length; i++)
+            {
+
+                for(int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+
+        }
+    }
+}
